f8a0760d7be0b7c9f93109369ab1203b734fd84e
corey.nadeau <djcjn16@hotmail.com>
2023-05-07 20:07:19 -0500

    second commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 
 function Contact() {
+    
   return (
-    <div>
-      <form class="w3-center w3-padding-64 text-white w3-opacity w3-black text-2xl" name="contact" id="contact" netlify>Contact Me
-    <p className='py-4'>
-      <label>Name: <input type="text" name="name" /></label>
-    </p>
-    <p className='pb-4'>
-      <label>Email: <input type="email" name="email" /></label>
-    </p>
-    <textarea name="message" id="message" cols="40" rows="5" placeholder="Write your comments or message here!"></textarea><br></br>
-    <p>
-      <button class="bg-black text-white rounded-lg p-2" type="submit">Send</button>
-    </p>
-    </form>
+    <div className='w3-black w3-opacity flex' id='contact'>
+    <div className="concert-container w3-container w3-content w3-padding-64">
+      <h1 className='text-4xl pb-10 justify-center flex'>Contact Me</h1>
+      <table className=''>
+        <tr>
+          <td>Email</td>
+          <td>coreynadeau1@gmail.com</td>
+        </tr>
+        <tr>
+          <td>Phone</td>
+          <td>(763) 600-3962</td>
+        </tr>
+      </table>
+    </div>
     </div>
   )
 }
